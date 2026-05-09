34bccba89881d2fe15565497bbd399c8e742ff6b
sfediuk <sfediuk@vector-software.com>
2026-05-09 20:33:21 +0300

    test initial commit

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,15 +1,15 @@
-import { Container } from '@mui/material';
-import { Route, Routes } from 'react-router-dom';
-import AppLayout from './components/AppLayout';
-import AboutPage from './pages/AboutPage';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
-import TasksPage from './pages/TasksPage';
+import { Container } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import TasksPage from "./pages/TasksPage";
 
 function App() {
   return (
     <AppLayout>
-      <Container maxWidth="lg" sx={{ py: 4 }}>
+      <Container maxWidth="lg" sx={{ py: 2 }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
